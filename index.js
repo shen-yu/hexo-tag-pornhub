0d63138863ee5a51b59217603250c6509ea6c83a
shenyu <448766534@qq.com>
2020-08-05 21:03:41 +0800

    fix

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ hexo.config.pornhub = Object.assign({
 }, hexo.config.pornhub);
 
 hexo.extend.tag.register('pornhub', (args) => {
-  var video_id = args[0];
-  var width = args[1] || hexo.config.pornhub.width;
-  var height = args[2] || hexo.config.pornhub.height;
-  var tag = `<iframe src=${BASE_URL}/${video_id} frameborder=0 width=${width} height=${height} scrolling=no allowfullscreen></iframe>`;
+  const video_id = args[0];
+  const width = args[1] || hexo.config.pornhub.width;
+  const height = args[2] || hexo.config.pornhub.height;
+  const tag = `<iframe src=${BASE_URL}/${video_id} frameborder=0 width=${width} height=${height} scrolling=no allowfullscreen></iframe>`;
   return tag;
 }, { async: true });
